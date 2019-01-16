96c5894373ccc52fceca58739f5c19b6f8518109
Linda <lindajzhao@gmail.com>
2019-01-15 21:25:40 -0500

    Normalize.css

--- app/js/imports.js
+++ app/js/imports.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/js/bootstrap.js";
-import "bootstrap/dist/css/bootstrap.css";
+import "normalize.css/normalize.css";
 import "../scss/style.scss";
 import "font-awesome/css/font-awesome.css";
